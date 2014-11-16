a08525ea290ff4edc766eda1ec80388be866a79e
Dan Albert <danalbert@google.com>
2014-11-15 18:37:07 -0800

    Move frameworks/ml to libc++.
    Bug: 15193147
    Change-Id: I88961fbd74340d2c862b6304cf9c47d50ab14ed5

--- bordeaux/learning/Android.mk
+++ bordeaux/learning/Android.mk
@@ -27,11 +27,7 @@ LOCAL_SHARED_LIBRARIES := libcutils liblog
 
 LOCAL_PRELINK_MODULE := false
 
-LOCAL_CFLAGS := -DANDROID
-
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../native
-
-include external/stlport/libstlport.mk
 
 include $(BUILD_SHARED_LIBRARY)
 
