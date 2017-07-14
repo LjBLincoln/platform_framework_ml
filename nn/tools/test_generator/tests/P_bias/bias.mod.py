dcd2fbf1da7ebdc1aa1b57c74db4fffd2911e3b8
I-Jui (Ray) Sung <ijsung@google.com>
2017-07-13 17:21:47 -0700

    First implementation of NNAPI test generator.
    Please see tools/test_generator/tests/P_full/addfloat.mod.py for how
    a test model may looks like.
    
    Bug: 63905942
    Test: tests/test.py on host
    Test: NeuralNetworksTest --gtest_filter=GeneratedTests.* on Angler
    
    Change-Id: Ia8592da130092248269ab3c8c58dcac184dc29ca

---
+++ nn/tools/test_generator/tests/P_bias/bias.mod.py
@@ -0,0 +1,9 @@
+# model
+i1 = Input("operand1","TENSOR_FLOAT32", "{3,4}")
+i2 = Input("operand2","TENSOR_FLOAT32", "{3,4}")
+i3 = Input("operand3","TENSOR_FLOAT32", "{3,4}")
+b1 = Bias("bias", "const Matrix3x4", "bias_arg", "TENSOR_FLOAT32", "{3,4}")
+o = Output("operand4","TENSOR_FLOAT32", "{3,4}")
+
+Model().Add(i1, i2).Add(i3, b1).Out(o)
+
