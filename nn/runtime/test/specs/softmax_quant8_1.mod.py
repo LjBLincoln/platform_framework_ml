f4ea576f3f90b5d73024b1f5bee49bdcb89958f7
Dong Chen <dongchen@google.com>
2017-09-13 19:13:37 -0700

    Add unit test for SOFTMAX op for quant8.
    Bug: 63905942
    Test: NeuralNetworksTest on Angler
    Change-Id: I6a785ccbb649ae196a7575c7b076dbb50fe50425

---
+++ nn/runtime/test/specs/softmax_quant8_1.mod.py
@@ -0,0 +1,17 @@
+# model
+model = Model()
+
+i1 = Input("input", "TENSOR_QUANT8_ASYMM", "0.0f, 127.5f, {1, 4}") # batch = 1, depth = 1
+beta = Float32Scalar("beta", 0.)
+output = Output("output", "TENSOR_QUANT8_ASYMM", "0.0f, 127.5f, {1, 4}")
+
+# model 1
+model = model.Operation("SOFTMAX", i1, beta).To(output)
+
+# Example 1. Input in operand 0,
+input0 = {i1: [1, 2, 10, 20]}
+
+output0 = {output: [0, 0, 0, 0]}
+
+# Instantiate an example
+Example((input0, output0))
