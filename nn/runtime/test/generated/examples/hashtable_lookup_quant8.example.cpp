7da2c9a54b25fd15546d6f99eba06dff1ac9a862
Yang Ni <yangni@google.com>
2017-09-29 14:07:17 -0700

    Updated data type for keys in Hashtable Lookup
    Bug: 63905942
    
    This OP was initially added when the NN API only allowed floating-
    point tensors.
    
    Now since integer tensors are allowed in NN API, update the Hashtable
    Lookup with the better suited data types.
    
    Test: NeuralNetworksTest
    Change-Id: I3dafb2f60bd428700a5a6de393b86dbd75f285dd

--- nn/runtime/test/generated/examples/hashtable_lookup_quant8.example.cpp
+++ nn/runtime/test/generated/examples/hashtable_lookup_quant8.example.cpp
@@ -4,19 +4,19 @@
 //Input(s)
 { // See tools/test_generator/include/TestHarness.h:MixedTyped
   // int -> FLOAT32 map
-  {{0, {123, 250, 255, 0}}, {1, {0, 123, 255}}},
-  // int -> INT32 map
   {},
+  // int -> INT32 map
+  {{0, {123, 250, 255, 0}}, {1, {0, 123, 255}}},
   // int -> QUANT8_ASYMM map
   {{2, {0, 1, 10, 11, 20, 21}}}
 },
 //Output(s)
 { // See tools/test_generator/include/TestHarness.h:MixedTyped
   // int -> FLOAT32 map
-  {{1, {1, 0, 1, 1}}},
+  {},
   // int -> INT32 map
   {},
   // int -> QUANT8_ASYMM map
-  {{0, {10, 11, 0, 0, 20, 21, 0, 1}}}
+  {{0, {10, 11, 0, 0, 20, 21, 0, 1}}, {1, {1, 0, 1, 1}}}
 }
 }, // End of an example
