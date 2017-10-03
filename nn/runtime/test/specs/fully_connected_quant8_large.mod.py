aba934df113efb34d727796b3ae959aebb9eb080
leozwang <leozwang@google.com>
2017-10-03 13:21:11 -0700

    Add fully_connected large test.
    Bug: 63905942
    
    Test: local test. NeuralNetworksTest on Pixel.
    Change-Id: I2543f7472b6064c3cb16297bb5b4ce4b83ba15ea

--- nn/runtime/test/specs/fully_connected_quant8_large.mod.py
+++ nn/runtime/test/specs/fully_connected_quant8_large.mod.py
@@ -15,20 +15,22 @@
 #
 
 model = Model()
-in0 = Input("op1", "TENSOR_FLOAT32", "{3, 1}")
-weights = Input("op2", "TENSOR_FLOAT32", "{1, 1}")
-bias = Input("b0", "TENSOR_FLOAT32", "{1}")
-out0 = Output("op3", "TENSOR_FLOAT32", "{3, 1}")
+in0 = Input("op1", "TENSOR_QUANT8_ASYMM", "{1, 5}, 0.2, 0") # batch = 1, input_size = 5
+weights = Input("op2", "TENSOR_QUANT8_ASYMM", "{1, 5}, 0.2, 0") # num_units = 1, input_size = 5
+bias = Input("b0", "TENSOR_QUANT8_ASYMM", "{1}, 0.04, 0")
+out0 = Output("op3", "TENSOR_QUANT8_ASYMM", "{1, 1}, 1.f, 0") # batch = 1, number_units = 1
 act = Int32Scalar("act", 0)
 model = model.Operation("FULLY_CONNECTED", in0, weights, bias, act).To(out0)
 
 # Example 1. Input in operand 0,
 input0 = {in0: # input 0
-             [2, 32, 16],
-         weights: [2],
-         bias: [4]}
+          [10, 10, 10, 10, 10],
+          weights:
+          [10, 20, 20, 20, 10],
+          bias:
+          [10]}
 output0 = {out0: # output 0
-               [8, 68, 36]}
+           [32]}
 
 # Instantiate an example
 Example((input0, output0))
