b57935d145fe920f3d80c5d2ecabf7b5df56773c
Mika Raento <mikie@google.com>
2018-04-17 18:55:53 +0100

    Fix slicing.sh for new directory structure
    Fix path handling in slicing.sh and helpers to work with versioned test
    directories.
    
    Added "set -eu" and a number of other checks to scripts to make them
    easier to debug in the future.
    
    Renegerated CTS tests with new scripts. The changes were not due to
    script changes in this CL, but due to not always running the scripts
    when changing models.
    
    TODO: set up a continuous build for slicing.
    
    Bug: 73560182
    Test: ./slicing.sh 2 V1_0/mobilenet_quantized.mod.py
    Test: ./runtime/test/specs/generate_test.sh and NeuralNetworksTest_static
    Change-Id: I9cc00df1c10fc369410388d63c34689647c5c56b
    Merged-In: I9cc00df1c10fc369410388d63c34689647c5c56b
    (cherry picked from commit da754e01b78a20ba119ed59a55ee63619dbc1732)

--- nn/runtime/test/specs/slicing.sh
+++ nn/runtime/test/specs/slicing.sh
@@ -21,40 +21,53 @@ if [[ $# -ne 2 || "$1" -eq "-h" || !(-e $2) || $1 -lt 1 ]]; then
   exit 1
 fi
 
+set -eu
+
+NO_OF_OPERATIONS=$1
+INPUT=$2
+SLICE=$ANDROID_BUILD_TOP/frameworks/ml/nn/tools/test_generator/slicing.py
+DIR_AND_NAME=${INPUT/.mod.py/}
+BASENAME=$(basename $DIR_AND_NAME)
+MODEL_ONLY=${DIR_AND_NAME}_sliced.model_only.py
+INPUT_ONLY=${DIR_AND_NAME}_sliced.input_only.py
+REFERENCE=${DIR_AND_NAME}_sliced.ref.py
+FINAL=${DIR_AND_NAME}_sliced_$1.mod.py
+SAVED_OUTPUT_FILE=/data/local/tmp/current_nnapi_example.example.py
+
+set +eu
 source $ANDROID_BUILD_TOP/build/envsetup.sh > /dev/null
+lunch $TARGET_PRODUCT
+set -eu
 source $ANDROID_BUILD_TOP/frameworks/ml/nn/runtime/test/specs/generate_test.sh
 
-SLICE=$ANDROID_BUILD_TOP/frameworks/ml/nn/tools/test_generator/slicing.py
-BASENAME=`basename -s .mod.py $2`
-MODEL_ONLY=${BASENAME}_sliced.model_only.py
-INPUT_ONLY=${BASENAME}_sliced.input_only.py
-REFERENCE=${BASENAME}_sliced.ref.py
-FINAL=${BASENAME}_sliced_$1.mod.py
-SAVED_OUTPUT_FILE=/data/local/tmp/current_nnapi_example.example.py
-$SLICE $2 -n $1 -m $MODEL_ONLY -e $INPUT_ONLY
-if [ $? -ne 0 ]; then
-  echo Error: Failed slicing $2
-  exit $?
-fi
+$SLICE $INPUT -n $NO_OF_OPERATIONS -m $MODEL_ONLY -e $INPUT_ONLY
 
 # create a temporary spec from the model and the input-only example
-echo "collecting_data = True" > ${BASENAME}_tmp.mod.py
-cat $MODEL_ONLY $INPUT_ONLY >> ${BASENAME}_tmp.mod.py
-generate_wrapper "log" $SAVED_OUTPUT_FILE ${BASENAME}_tmp.mod.py
+echo "collecting_data = True" > ${DIR_AND_NAME}_tmp.mod.py
+cat $MODEL_ONLY $INPUT_ONLY >> ${DIR_AND_NAME}_tmp.mod.py
+generate_wrapper "log" $SAVED_OUTPUT_FILE ${DIR_AND_NAME}_tmp.mod.py
 
 # execute the sliced testcase and collect reference outputs
 TMP_EXEC=$(adb shell mktemp --tmpdir /data/local/tmp)
-HOST_EXEC_DIR=$ANDROID_PRODUCT_OUT/data/nativetest64/NeuralNetworksTest/
-adb remount && mm -j40 > /dev/null && \
-  adb push ${HOST_EXEC_DIR}/NeuralNetworksTest $TMP_EXEC && \
-  adb shell $TMP_EXEC --gtest_filter="*.${BASENAME}_tmp" \
-  &&  adb pull $SAVED_OUTPUT_FILE $REFERENCE
+HOST_EXEC_DIR=$ANDROID_PRODUCT_OUT/data/nativetest64/NeuralNetworksTest_static/
+if [[ ! -d $HOST_EXEC_DIR ]]; then
+  HOST_EXEC_DIR=$ANDROID_PRODUCT_OUT/data/nativetest/NeuralNetworksTest_static/
+fi
+[[ -d $HOST_EXEC_DIR ]] || (echo "cannot find $HOST_EXEC_DIR"; exit 1)
+
+set +u
+adb remount
+mm -j40
+adb push ${HOST_EXEC_DIR}/NeuralNetworksTest_static $TMP_EXEC
+adb shell $TMP_EXEC --gtest_filter="*.${BASENAME}_tmp"
+adb pull $SAVED_OUTPUT_FILE $REFERENCE
+set -u
 GENERATED=$ANDROID_BUILD_TOP/frameworks/ml/nn/runtime/test/generated/
 
 # remove temporary spec and corresponding generated files
-rm -f ${BASENAME}_tmp.mod.py
-rm -f ${GENERATED}/models/${BASENAME}_tmp.model.cpp
-rm -f ${GENERATED}/examples/${BASENAME}_tmp.example.cpp
+rm ${DIR_AND_NAME}_tmp.mod.py
+rm ${GENERATED}/models/${BASENAME}_tmp.model.cpp
+rm ${GENERATED}/examples/${BASENAME}_tmp.example.cpp
 
 if [ $? -ne 0 ]; then
   echo Error: Failed building intermediate model for $2
